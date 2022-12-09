9de9fda125f528ee2b4466a8693ba724840b6252
RyanSantos5 <santosryan131@gmail.com>
2022-12-09 09:37:58 -0300

    backup

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM node:alpine
 WORKDIR /usr/app
 
 COPY package*.json ./
+COPY yarn*.lock ./
 RUN npm install
+RUN yarn
 
 COPY . .
 
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();  
 
 app.get('/',(req,res)=> {
-    res.send('Hello World 5');
+    res.send('Hello World Teste');
 });
 
 app.listen(PORT,HOST);
